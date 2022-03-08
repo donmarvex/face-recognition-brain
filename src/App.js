9ac70840a979516bc54a8eb90646dba3883b77b9
DonMarvex <marvelous.emoghene@gmail.com>
2022-03-08 14:46:01 +0100

    Initialize with create react app, updates and components

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        <ImageLinkForm />
+        {/*<FaceRecognition /> */}
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import brain from './brain.png';
+import './Logo.css';
+
+const Logo = () => {
+  return (
+    <div className='ma4 mt0'>
+      <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150}}>
+        <div className="Tilt-inner pa4"> 
+          <img style={{ paddingTop: '2px' }} alt="logo" src={brain}/> 
+        </div>
+      </Tilt>
+
+    </div>
+  );
+}
+
+export default Logo;
