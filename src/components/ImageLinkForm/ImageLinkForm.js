fc9f8c49e4922d8b3ffbac51e1db606463467505
Marvelous Emoghene <marvelous.emoghene@gmail.com>
2022-04-01 10:54:34 +0100

    updated rank component, minor fixes

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './ImageLinkForm.css';
 
 // instead of props.onInputChange --> destructure
-const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
+const ImageLinkForm = ({ onInputChange, onPictureSubmit }) => {
   return (
     <div>
       <p className='f3'>  
@@ -11,7 +11,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
       <div className='center'>
           <div className='center form pa4 br3 shadow-5'>
             <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} />
-            <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' onClick={onButtonSubmit}>Detect</button>
+            <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' onClick={onPictureSubmit}>Detect</button>
           </div>
       </div>
     </div>
